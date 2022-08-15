edd8bbc1bdc3b98cafedac2bb946e4856aa1c100
Gede Rico Wijaya <gricowijaya@gmail.com>
2022-08-15 11:42:22 +0800

    add user own declarative types

---
+++ your-own-types/main.go
@@ -0,0 +1,17 @@
+package main 
+
+import ("fmt")
+
+var a int
+
+type hotdog int // create our own type  but the type of int is not hotdog but the type hotdog is int
+
+func main() {
+  b := 1;
+
+  b +=  1 ;
+
+  b -= b;
+}
+
+
