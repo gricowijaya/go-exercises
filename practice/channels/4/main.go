03503e974de65b4f483b2665a540e6422fa3d26e
Gede Rico Wijaya <gricowijaya@gmail.com>
2022-10-12 13:03:13 +0800

    add fourth_practice_using_channels

---
+++ practice/channels/4/main.go
@@ -0,0 +1,41 @@
+package main
+
+import (
+	"fmt"
+)
+
+func main() {
+	q := make(chan int)
+	c := gen(q)
+
+	receive(c, q)
+
+	fmt.Println("about to exit")
+}
+
+// create the function that receive two channels which of type receive channels
+func receive (c, q <-chan int) { 
+    for {
+        select {
+        case  v := <-c:
+            fmt.Println(v)
+        case  <-q:
+            return ;
+        }
+    }
+}
+
+// receive only channel 
+func gen(q <-chan int) <-chan int {
+	c := make(chan int)
+
+    go func() {
+        for i := 0; i < 100; i++ {
+            c <- i
+        }
+        q <- 1
+        close(c)
+    }()
+
+	return c
+}
