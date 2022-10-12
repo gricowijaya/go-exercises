3c4ca6da3bfdd6c7337172f2390ec6a0cb0a37b9
Gede Rico Wijaya <gricowijaya@gmail.com>
2022-10-12 12:06:15 +0800

    add channel challenge

--- practice/channels/2/first.go
+++ practice/channels/2/first.go
@@ -5,13 +5,16 @@ import (
 )
 
 func main() {
-	cr := make(<-chan int)
+	cr := make(chan int)
 
+    // the process order with using anonymous go routine 
 	go func() {
 		cr <- 42
 	}()
+    
 	fmt.Println(<-cr)
 
 	fmt.Printf("------\n")
 	fmt.Printf("cr\t%T\n", cr)
+
 }
